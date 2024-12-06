362b649d085f440a77af5aa9c03f5c872016b4c7
Irina Abraeva <abraeva.98@mail.ru>
2024-12-06 06:57:32 -0500

    cascade

--- src/main/java/hibernate_test2/entity/Employee.java
+++ src/main/java/hibernate_test2/entity/Employee.java
@@ -4,15 +4,11 @@
  */
 package hibernate_test.entity;
 
-import hibernate_test2.entity.Detail;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 /**
