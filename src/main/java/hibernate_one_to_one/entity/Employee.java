d40bd9e550a60aa540d5e791b7d740dc7cf0c353
Irina Abraeva <abraeva.98@mail.ru>
2024-12-06 20:20:46 -0500

    one-to-many

--- src/main/java/hibernate_one_to_one/entity/Employee.java
+++ src/main/java/hibernate_one_to_one/entity/Employee.java
@@ -4,15 +4,11 @@
  */
 package hibernate_test.entity;
 
-import hibernate_one_to_one.entity.Detail;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 /**
